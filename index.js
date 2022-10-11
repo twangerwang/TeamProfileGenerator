7231ba5af4312f4e41b56955a997ccbe48b52220
twangerwang <108110493+twangerwang@users.noreply.github.com>
2022-10-11 11:57:02 -0500

    preliminary files/function created

--- index.js
+++ index.js
@@ -1,7 +1,136 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateHTML = require('./src/generateHTML');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 
-const generateHTML = ({ name, location, github, linkedin }) =>
+const team = [];
+
+const managerQuestions = [
+  {
+    type:'input',
+    name: 'name',
+    message: 'What is your name?',
+  },
+  {
+    type: 'input',
+    name: 'id',
+    message: 'What is your ID?',
+  },
+  {
+    type:'input',
+    name: 'email',
+    message: 'What is your email address?',
+  },
+  {
+    type:'input',
+    name: 'officeNumber',
+    message: 'What is your office number?',
+  }
+]
+
+const engineerQuestions = [
+  {
+    type:'input',
+    name: 'name',
+    message: 'What is your name?',
+  },
+  {
+    type: 'input',
+    name: 'id',
+    message: 'What is your ID?',
+  },
+  {
+    type:'input',
+    name: 'email',
+    message: 'What is your email address?',
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub username?',
+  }
+]
+
+const internQuestions = [
+  {
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?',
+  },
+  {
+    type: 'input',
+    name: 'id',
+    message: 'What is your ID?',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?',
+  },
+  {
+    type: 'input',
+    name: 'school',
+    message: 'Wherer did you go to school?',
+  }
+]
+
+const newMember = [
+  {
+    type:'list',
+    name:'memberType',
+    message: 'Which type of team member would you like to add?',
+    choices: ['Engineer', 'Intern', 'None'],
+  }
+]
+
+function addMember(questions, type) {
+  let uniqueQuestion;
+  inquirer.prompt(questions)
+    .then((answers)=>{
+      switch(type){
+        case Engineer:
+          uniqueQuestion = answers.github;
+          break;
+        case Intern:
+          uniqueQuestion = answers.school;
+          break;
+        default:
+          uniqueQuestion = answers.officeNumber;
+          break;
+      }
+      const newEmployee = new type(answers.name, answers.id, answers.email, uniqueQuestions)
+      team.push(newEmployee)
+      additionalMembers()
+    })
+}
+
+function additionalMembers() {
+  inquirer.prompt(newMember)
+  .then((answers)=>{
+    switch(answers.memberType){
+      case Engineer:
+        addMember(engineerQuestions, Engineer);
+        break;
+      case Intern:
+        addMember(internQuestions, Intern);
+        break;
+      default:
+        //call function to generate html
+      const outputHTML = generateHTML(team);
+      fs.writeFile('./dist.team.html', outputHTML, (err)=>
+      err? console.log(err):console.log('HTML created successfully!'))
+    }
+  })
+}
+
+
+const readmeFile = generateReadme(answers);
+fs.writeFile('./utils/readme.md', readmeFile, (err)=>
+err? console.log(err):console.log('readme.md successfully created!')
+);
+
   `<!DOCTYPE html>
 <html lang="en">
 <head>
